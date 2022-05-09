27b99ac5b981fb2c200158b436eee223418b5e00
hhhhhq <Mr.j@tom.com>
2022-05-10 06:46:56 +0800

    init

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'img',
+            name: '[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin()
+  ]
+}
